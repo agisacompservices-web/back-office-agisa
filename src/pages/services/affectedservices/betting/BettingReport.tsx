0a5e93ec20b511a417ae5861f147d335579cf243
dolphfi <rodolphfidele@gmail.com>
2026-02-25 15:36:26 -0500

    fix CardDescription

--- src/pages/services/affectedservices/betting/BettingReport.tsx
+++ src/pages/services/affectedservices/betting/BettingReport.tsx
@@ -4,8 +4,7 @@ import {
     Card,
     CardContent,
     CardHeader,
-    CardTitle,
-    CardDescription
+    CardTitle
 } from "../../../../components/ui/card";
 import { Button } from "../../../../components/ui/button";
 import { Input } from "../../../../components/ui/input";
